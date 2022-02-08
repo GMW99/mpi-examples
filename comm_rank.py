d78854a02795d1d23c535ba44654347dbfc0801e
GMW99 <gabryelmw@gmail.com>
2022-02-08 10:18:05 +0000

    Basic comm_rank

---
+++ comm_rank.py
@@ -0,0 +1,13 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD  # Default communicator in MPI. Groups processes together all are connected.
+proc_nom = comm.Get_rank()  # Current process number.
+nom_procs = comm.Get_size()  # How many processors there are.
+
+# Print to console
+print("\n Starting processes %d out of %d" % (proc_nom, nom_procs))
+
+# Write to a file
+f = open("hello.txt", "a")
+f.write("Starting processes %d out of %d \r\n" % (proc_nom, nom_procs))
+f.close()
